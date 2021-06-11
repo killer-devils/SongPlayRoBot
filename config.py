a1091e30bb60f381875516a00f27fef95971e211
killer-devils <85469679+killer-devils@users.noreply.github.com>
2021-06-11 21:56:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "5670781"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "47b9325210779c974bf3f0147cc578d1" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1810069117:AAFwjw6OIBb-rL8JH1fSSROpl0Zb7kbBgqg" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
